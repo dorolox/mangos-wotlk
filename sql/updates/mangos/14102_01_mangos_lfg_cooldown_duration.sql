a3ddfa0adc787c6aa1766d200378dcc690602e97
Dorolox <dorolox@gmail.com>
2026-06-22 20:30:35 +0200

    Reduce lfg re-tag penality

---
+++ sql/updates/mangos/14102_01_mangos_lfg_cooldown_duration.sql
@@ -0,0 +1,4 @@
+-- Reduce LFG dungeon deserter (71041) and dungeon cooldown (71328) durations to 1 minute
+-- Default: Dungeon Deserter = 15 min (DurationIndex 30), Dungeon Cooldown = 30 min (DurationIndex 347)
+-- DurationIndex 39 = 60000ms = 1 minute
+UPDATE `spell_template` SET `duration_index` = 39 WHERE `id` IN (71041, 71328);
